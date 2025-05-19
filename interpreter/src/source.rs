582df6d61e876b49348de70c95768ec4957a8233
Sam Cutter <swcutter@pm.me>
2025-05-19 19:27:29 +0100

    Added the Location struct to store source code locations

--- interpreter/src/source.rs
+++ interpreter/src/source.rs
@@ -1,34 +1,40 @@
+pub struct Location {
+    pub index: usize,
+    pub line: usize,
+    pub column: usize,
+}
+
 pub struct Source {
     text: Vec<char>,
-    current: usize,
-    line: usize,
-    column: usize,
+    location: Location,
 }
 
 impl Source {
     pub fn new(text: &str) -> Self {
         Self {
             text: text.chars().collect(),
-            current: 0,
-            line: 0,
-            column: 0,
+            location: Location {
+                index: 0,
+                line: 0,
+                column: 0,
+            },
         }
     }
 
     pub fn peek(&self) -> Option<char> {
-        self.text.get(self.current).copied()
+        self.text.get(self.location.index).copied()
     }
 
     pub fn advance(&mut self) -> Option<char> {
         let next = self.peek();
 
         if let Some(character) = next {
-            self.current += 1;
-            self.column += 1;
+            self.location.index += 1;
+            self.location.column += 1;
 
             if character == '\n' {
-                self.line += 1;
-                self.column = 0;
+                self.location.line += 1;
+                self.location.column = 0;
             }
         }
 
