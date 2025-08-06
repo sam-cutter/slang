8d906767df8db6cd95b1b5bfcd4943ddec436a33
Sam Cutter <swcutter@pm.me>
2025-08-06 23:35:06 +0200

    Documented the source module.

--- interpreter/src/source.rs
+++ interpreter/src/source.rs
@@ -1,9 +1,13 @@
 use std::fmt::Display;
 
+/// Represents the location of a character within a source code string.
 #[derive(Clone, Copy, Debug)]
 pub struct Location {
+    /// The zero-indexed position of the character.
     pub index: usize,
+    /// The line (`>= 1`) which the character appears on.
     pub line: usize,
+    /// The column (`>= 1`) which the character appears in.
     pub column: usize,
 }
 
@@ -14,6 +18,7 @@ impl Display for Location {
 }
 
 impl Location {
+    /// Returns a location representing the first character in a source code string, with index `0`, line `1`, column `1`.
     pub fn start() -> Self {
         Self {
             index: 0,
@@ -23,8 +28,11 @@ impl Location {
     }
 }
 
+/// Can represent either a concrete location, or the end of a source code string.
 pub enum GeneralLocation {
+    /// A concrete location in the source code.
     Location(Location),
+    /// The end of the source code string.
     EndOfFile,
 }
 
@@ -37,12 +45,16 @@ impl Display for GeneralLocation {
     }
 }
 
+/// A wrapper around the source code string.
 pub struct Source {
+    /// The source code string.
     text: Vec<char>,
+    /// The location of the next character.
     location: Location,
 }
 
 impl Source {
+    /// Creates a new [Source] from a string input.
     pub fn new(text: &str) -> Self {
         Self {
             text: text.chars().collect(),
@@ -50,14 +62,17 @@ impl Source {
         }
     }
 
+    /// Returns the next character in the string, without advancing the position.
     pub fn peek(&self) -> Option<char> {
         self.text.get(self.location.index).copied()
     }
 
+    /// Returns the (next + 1)th character in the string, without advancing the position.
     pub fn peek_after(&self) -> Option<char> {
         self.text.get(self.location.index + 1).copied()
     }
 
+    /// Returns the next character in the string, and advances the position.
     pub fn advance(&mut self) -> Option<char> {
         let next = self.peek();
 
@@ -74,6 +89,9 @@ impl Source {
         next
     }
 
+    /// Conditionally advances the position if the next character matches a target.
+    ///
+    /// If the next character is equal to `target`, then the position is advanced, and `true` is returned. Otherwise, the position is not advanced, and `false` is returned.
     pub fn matches(&mut self, target: char) -> bool {
         if let Some(character) = self.peek() {
             if character == target {
@@ -86,10 +104,12 @@ impl Source {
         false
     }
 
+    /// Returns whether the source code string has been consumed fully.
     pub fn at_end(&self) -> bool {
         self.location.index >= self.text.len()
     }
 
+    /// Returns the location of the next character.
     pub fn location(&self) -> Location {
         self.location
     }
